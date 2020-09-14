96778821292a3bd4a59449b7ae490aedc6921180
Eric Kemp <eric.kemp@nasa.gov>
2020-09-14 11:21:03 -0400

    Added support for V1901 APHRODITE-2 precipitation data.
    Also updated interpolation for APHRODITE, added consistency check for
    missing data value, and did some code cleanup.

--- lvt/datastreams/APHRO_PRCP/APHROPRCP_obsMod.F90
+++ lvt/datastreams/APHRO_PRCP/APHROPRCP_obsMod.F90
@@ -1,40 +1,41 @@
-!-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------------
+!-----------------------BEGIN NOTICE -- DO NOT EDIT----------------------------
 ! NASA GSFC Land surface Verification Toolkit (LVT) V1.0
-!-------------------------END NOTICE -- DO NOT EDIT-----------------------------
+!-------------------------END NOTICE -- DO NOT EDIT----------------------------
 !BOP
-! 
+!
 ! !MODULE: APHROPRCP_obsMod
 ! \label(APHROPRCP_obsMod)
 !
 ! !INTERFACE:
 module APHROPRCP_obsMod
-! 
-! !USES:   
+!
+! !USES:
   use ESMF
 
   implicit none
-  PRIVATE 
+  PRIVATE
 !
-! !INPUT PARAMETERS: 
-! 
+! !INPUT PARAMETERS:
+!
 ! !OUTPUT PARAMETERS:
 !
-! !DESCRIPTION: 
-!  This module handles the observation plugin for the 
-!  APHRODITE (Asian Precipitaton - Highly-Resolved 
+! !DESCRIPTION:
+!  This module handles the observation plugin for the
+!  APHRODITE (Asian Precipitaton - Highly-Resolved
 !  Observational Data Integration Towards Evaluation)
-!  daily gridded precipitation data. 
+!  daily gridded precipitation data.
 !
 !  https://climatedataguide.ucar.edu/climate-data/aphrodite-asian-precipitation-highly-resolved-observational-data-integration-towards
-!  
-!  Coverage is from 1951 - 2007, daily at 
+!
+!  Coverage is from 1951 - 2007, daily at
 !  0.25 deg lat/lon resolution
-! 
+!
 ! !FILES USED:
 !
-! !REVISION HISTORY: 
+! !REVISION HISTORY:
 !  09 Aug 2017   Sujay Kumar  Initial Specification
-! 
+!  14 Sep 2020   Eric Kemp    Added V1901 netCDF support, valid 1998-2015.
+!                             Updated interpolation.
 !EOP
 
   PUBLIC :: APHROPRCP_obsinit
@@ -43,37 +44,56 @@ module APHROPRCP_obsMod
   type, public :: aphroprcpobsdec
      character*100        :: odir
      character*100        :: loc
+     character*100        :: version ! EMK
      integer              :: nc, nr
      integer              :: yr
      type(ESMF_Time)         :: startTime
      type(ESMF_TimeInterval) :: timeStep
-     
+
      real,    allocatable     :: rlat(:)
      real,    allocatable     :: rlon(:)
-     integer, allocatable     :: n11(:)
-     integer, allocatable     :: n12(:)
-     integer, allocatable     :: n21(:)
-     integer, allocatable     :: n22(:)
-     real,    allocatable     :: w11(:)
-     real,    allocatable     :: w12(:)
-     real,    allocatable     :: w21(:)
-     real,    allocatable     :: w22(:)
+
+     ! EMK Updated interpolation
+     ! integer, allocatable     :: n11(:)
+     ! integer, allocatable     :: n12(:)
+     ! integer, allocatable     :: n21(:)
+     ! integer, allocatable     :: n22(:)
+     ! real,    allocatable     :: w11(:)
+     ! real,    allocatable     :: w12(:)
+     ! real,    allocatable     :: w21(:)
+     ! real,    allocatable     :: w22(:)
+
+     ! EMK Used for upscale averaging
+     integer, allocatable        :: n11(:)
+
+     ! Used with budget interpolation (precip)
+     integer, allocatable        :: n112(:,:)
+     integer, allocatable        :: n122(:,:)
+     integer, allocatable        :: n212(:,:)
+     integer, allocatable        :: n222(:,:)
+     real,    allocatable        :: w112(:,:)
+     real,    allocatable        :: w122(:,:)
+     real,    allocatable        :: w212(:,:)
+     real,    allocatable        :: w222(:,:)
+
+     real :: datares
+
      real,    allocatable     :: rainf(:,:,:)
   end type aphroprcpobsdec
 
   type(aphroprcpobsdec), allocatable :: aphroprcpobs(:)
 
 contains
-  
+
 !BOP
-! 
+!
 ! !ROUTINE: APHROPRCP_obsInit
 ! \label{APHROPRCP_obsInit}
 !
-! !INTERFACE: 
+! !INTERFACE:
   subroutine APHROPRCP_obsinit(i)
-! 
-! !USES: 
+!
+! !USES:
     use ESMF
     use LVT_coreMod
     use LVT_histDataMod
@@ -82,25 +102,25 @@ contains
 
     implicit none
 !
-! !INPUT PARAMETERS: 
-    integer,   intent(IN) :: i 
-! 
+! !INPUT PARAMETERS:
+    integer,   intent(IN) :: i
+!
 ! !OUTPUT PARAMETERS:
 !
-! !DESCRIPTION: 
+! !DESCRIPTION:
 !  This subroutine initializes and sets up the data structures required
-!  for reading APHRO PCP data. 
-! 
+!  for reading APHRO PCP data.
+!
 ! !FILES USED:
 !
-! !REVISION HISTORY: 
-! 
+! !REVISION HISTORY:
+!
 !EOP
 
     real               :: gridDesci(50)
     integer            :: status
-    
-    if(.not.allocated(aphroprcpobs)) then 
+
+    if(.not.allocated(aphroprcpobs)) then
        allocate(aphroprcpobs(LVT_rc%nDataStreams))
     endif
 
@@ -110,7 +130,7 @@ contains
 
     call ESMF_ConfigGetAttribute(LVT_config, aphroprcpobs(i)%loc, &
          label='APHRO PCP data region:',rc=status)
-    if(status.ne.0) then 
+    if(status.ne.0) then
        write(LVT_logunit,*) '[ERR] APHRO PCP data region: not defined'
        write(LVT_logunit,*) '[ERR] options are: '
        write(LVT_logunit,*) "[ERR] 'MA' (for Monsoon Asia)' "
@@ -120,56 +140,113 @@ contains
        call LVT_endrun()
     endif
 
-    gridDesci = 0 
+    ! EMK Add version number
+    call ESMF_ConfigGetAttribute(LVT_config, aphroprcpobs(i)%version, &
+         label='APHRO PCP version:',rc=status)
+    if(status.ne.0) then
+       write(LVT_logunit,*) '[ERR] APHRO PCP version: not defined'
+       write(LVT_logunit,*) '[ERR] options are: '
+       write(LVT_logunit,*) "[ERR] 'V1101' Original Release (1951-2007)"
+       write(LVT_logunit,*) "[ERR] 'V1901' (1998-2015) "
+       call LVT_endrun()
+    endif
+
+
+    gridDesci = 0
     call LVT_update_timestep(LVT_rc, 86400)
 
     allocate(aphroprcpobs(i)%rlat(LVT_rc%lnc*LVT_rc%lnr))
     allocate(aphroprcpobs(i)%rlon(LVT_rc%lnc*LVT_rc%lnr))
 
-    allocate(aphroprcpobs(i)%w11(LVT_rc%lnc*LVT_rc%lnr))
-    allocate(aphroprcpobs(i)%w12(LVT_rc%lnc*LVT_rc%lnr))
-    allocate(aphroprcpobs(i)%w21(LVT_rc%lnc*LVT_rc%lnr))
-    allocate(aphroprcpobs(i)%w22(LVT_rc%lnc*LVT_rc%lnr))
+    ! EMK...Update Interpolation
+    ! allocate(aphroprcpobs(i)%w11(LVT_rc%lnc*LVT_rc%lnr))
+    ! allocate(aphroprcpobs(i)%w12(LVT_rc%lnc*LVT_rc%lnr))
+    ! allocate(aphroprcpobs(i)%w21(LVT_rc%lnc*LVT_rc%lnr))
+    ! allocate(aphroprcpobs(i)%w22(LVT_rc%lnc*LVT_rc%lnr))
 
-    allocate(aphroprcpobs(i)%n11(LVT_rc%lnc*LVT_rc%lnr))
-    allocate(aphroprcpobs(i)%n12(LVT_rc%lnc*LVT_rc%lnr))
-    allocate(aphroprcpobs(i)%n21(LVT_rc%lnc*LVT_rc%lnr))
-    allocate(aphroprcpobs(i)%n22(LVT_rc%lnc*LVT_rc%lnr))
+    ! allocate(aphroprcpobs(i)%n11(LVT_rc%lnc*LVT_rc%lnr))
+    ! allocate(aphroprcpobs(i)%n12(LVT_rc%lnc*LVT_rc%lnr))
+    ! allocate(aphroprcpobs(i)%n21(LVT_rc%lnc*LVT_rc%lnr))
+    ! allocate(aphroprcpobs(i)%n22(LVT_rc%lnc*LVT_rc%lnr))
 
-    if(aphroprcpobs(i)%loc.eq."MA") then 
+    if(aphroprcpobs(i)%loc.eq."MA") then
        aphroprcpobs(i)%nc = 360
        aphroprcpobs(i)%nr = 280
-       
-       gridDesci(1) = 0 
+
+       aphroprcpobs(i)%datares = 0.25 ! deg
+
+       gridDesci(1) = 0
        gridDesci(2) = 360
-       gridDesci(3) = 280 
+       gridDesci(3) = 280
        gridDesci(4) = -14.875
        gridDesci(5) = 60.125
-       gridDesci(6) = 128 
+       gridDesci(6) = 128
        gridDesci(7) = 54.875
        gridDesci(8) = 149.875
        gridDesci(9) = 0.25
        gridDesci(10) = 0.25
        gridDesci(20) = 64
-       
-       call bilinear_interp_input(gridDesci,LVT_rc%gridDesc,&
-            LVT_rc%lnc*LVT_rc%lnr, &
-            aphroprcpobs(i)%rlat, aphroprcpobs(i)%rlon, &  
-            aphroprcpobs(i)%n11, aphroprcpobs(i)%n12,   & 
-            aphroprcpobs(i)%n21, aphroprcpobs(i)%n22,   & 
-            aphroprcpobs(i)%w11, aphroprcpobs(i)%w12,   & 
-            aphroprcpobs(i)%w21, aphroprcpobs(i)%w22)
+
+       ! EMK Updated interpolation
+       !call bilinear_interp_input(gridDesci,LVT_rc%gridDesc,&
+       !     LVT_rc%lnc*LVT_rc%lnr, &
+       !     aphroprcpobs(i)%rlat, aphroprcpobs(i)%rlon, &
+       !     aphroprcpobs(i)%n11, aphroprcpobs(i)%n12,   &
+       !     aphroprcpobs(i)%n21, aphroprcpobs(i)%n22,   &
+       !     aphroprcpobs(i)%w11, aphroprcpobs(i)%w12,   &
+       !     aphroprcpobs(i)%w21, aphroprcpobs(i)%w22)
+
+       ! EMK Use budget-bilinear interpolation if APHRODITE is at coarser
+       ! resolution than the analysis grid; otherwise, use upscale averaging.
+       if (LVT_isAtAFinerResolution(aphroprcpobs(i)%datares)) then
+
+          allocate(aphroprcpobs(i)%n112(LVT_rc%lnc*LVT_rc%lnr, 25))
+          allocate(aphroprcpobs(i)%n122(LVT_rc%lnc*LVT_rc%lnr, 25))
+          allocate(aphroprcpobs(i)%n212(LVT_rc%lnc*LVT_rc%lnr, 25))
+          allocate(aphroprcpobs(i)%n222(LVT_rc%lnc*LVT_rc%lnr, 25))
+          allocate(aphroprcpobs(i)%w112(LVT_rc%lnc*LVT_rc%lnr, 25))
+          allocate(aphroprcpobs(i)%w122(LVT_rc%lnc*LVT_rc%lnr, 25))
+          allocate(aphroprcpobs(i)%w212(LVT_rc%lnc*LVT_rc%lnr, 25))
+          allocate(aphroprcpobs(i)%w222(LVT_rc%lnc*LVT_rc%lnr, 25))
+
+          aphroprcpobs(i)%n112 = 0
+          aphroprcpobs(i)%n122 = 0
+          aphroprcpobs(i)%n212 = 0
+          aphroprcpobs(i)%n222 = 0
+          aphroprcpobs(i)%w112 = 0
+          aphroprcpobs(i)%w122 = 0
+          aphroprcpobs(i)%w212 = 0
+          aphroprcpobs(i)%w222 = 0
+
+          call conserv_interp_input(gridDesci,LVT_rc%gridDesc,&
+               LVT_rc%lnc*LVT_rc%lnr, &
+               aphroprcpobs(i)%rlat, aphroprcpobs(i)%rlon,&
+               aphroprcpobs(i)%n112, aphroprcpobs(i)%n122, &
+               aphroprcpobs(i)%n212, aphroprcpobs(i)%n222, &
+               aphroprcpobs(i)%w112, aphroprcpobs(i)%w122, &
+               aphroprcpobs(i)%w212, aphroprcpobs(i)%w222)
+
+       else
+
+          allocate(aphroprcpobs(i)%n11(aphroprcpobs(i)%nc*aphroprcpobs(i)%nr))
+          aphroprcpobs(i)%n11 = 0
+
+          call upscaleByAveraging_input(gridDesci, LVT_rc%gridDesc,&
+               aphroprcpobs(i)%nc*aphroprcpobs(i)%nr, &
+               LVT_rc%lnc*LVT_rc%lnr, &
+               aphroprcpobs(i)%n11)
+       end if
     else
        write(LVT_logunit,*) "[ERR] The Aphrodite plugin only supports"
        write(LVT_logunit,*) "[ERR] the MA region currently"
        call LVT_endrun()
     endif
-    
+
     allocate(aphroprcpobs(i)%rainf(aphroprcpobs(i)%nc,&
          aphroprcpobs(i)%nr,&
          366))
     aphroprcpobs(i)%yr = -1
-    
+
     call ESMF_TimeSet(aphroprcpobs(i)%startTime,  yy=LVT_rc%yr, &
          mm = 1, &
          dd = 1, &
@@ -190,7 +267,7 @@ contains
 
     call ESMF_TimeIntervalSet(aphroprcpobs(i)%timestep, s=86400, rc=status)
     call LVT_verify(status, 'error in setting timestep (aphroprcpobs)')
-        
+
   end subroutine APHROPRCP_obsinit
 
 
--- lvt/datastreams/APHRO_PRCP/readAPHROPRCPObs.F90
+++ lvt/datastreams/APHRO_PRCP/readAPHROPRCPObs.F90
@@ -1,18 +1,18 @@
-!-----------------------BEGIN NOTICE -- DO NOT EDIT-----------------------------
+!-----------------------BEGIN NOTICE -- DO NOT EDIT----------------------------
 ! NASA GSFC Land surface Verification Toolkit (LVT) V1.0
-!-------------------------END NOTICE -- DO NOT EDIT-----------------------------
+!-------------------------END NOTICE -- DO NOT EDIT----------------------------
 #include "LVT_misc.h"
 !BOP
-! 
+!
 ! !ROUTINE: readAPHROPRCPobs
 ! \label{readAPHROPRCPobs}
 !
-! !INTERFACE: 
+! !INTERFACE:
 subroutine readAPHROPRCPobs(source)
-! 
-! !USES:   
+!
+! !USES:
   use ESMF
-  use LVT_coreMod,    only : LVT_rc, LVT_domain
+  use LVT_coreMod,    only : LVT_rc, LVT_domain, LVT_isAtAfinerResolution
   use LVT_histDataMod
   use LVT_logMod,     only : LVT_logunit, LVT_getNextUnitNumber, &
        LVT_releaseUnitNumber, LVT_verify, LVT_endrun
@@ -26,49 +26,52 @@ subroutine readAPHROPRCPobs(source)
 
   implicit none
 !
-! !INPUT PARAMETERS: 
+! !INPUT PARAMETERS:
   integer,  intent(in) :: source
-! 
+!
 ! !OUTPUT PARAMETERS:
 !
-! !DESCRIPTION: 
+! !DESCRIPTION:
 !  This subroutine reads and processes the gridded APHRO precip
-!  data. The data for a given year is read into memory at the 
+!  data. The data for a given year is read into memory at the
 !  start of a year and indexed into during each day.
-! 
+!
 ! !FILES USED:
 !
-! !REVISION HISTORY: 
+! !REVISION HISTORY:
 !  09 AUG 2017: Sujay Kumar, Initial Specification
-!  23 SEP 2019: Yeosang Yoon, Update file name for product for 2007-2015 
-!
+!  14 Sep 2020: Eric Kemp, Added V1901 support.  Updated interpolation, and
+!                 added consistent check for missing data.
 !EOP
 
-  integer             :: ftn 
+  integer             :: ftn
   character*100       :: filename
   character*4         :: fyr
   logical             :: file_exists
   real, allocatable   :: rainf(:,:,:)
-  real                :: prcp_in(aphroprcpobs(source)%nc*aphroprcpobs(source)%nr)
+  real                :: &
+       prcp_in(aphroprcpobs(source)%nc*aphroprcpobs(source)%nr)
   logical*1           :: lb(aphroprcpobs(source)%nc*aphroprcpobs(source)%nr)
   logical*1           :: lo(LVT_rc%lnc*LVT_rc%lnr)
   real                :: prcp_out(LVT_rc%lnc*LVT_rc%lnr)
   real                :: prcp_final(LVT_rc%lnc, LVT_rc%lnr)
-  integer             :: k,c,r,nt
-  integer             :: nid,varid
+  integer             :: k, c, r, nt, t
+  integer             :: nid, varid
   integer             :: iret
   type(ESMF_Time)     :: aphrotime1
   integer             :: status
   real                :: timenow
   logical             :: alarmCheck
+  real                :: fill_value ! EMK
+  real                :: go(LVT_rc%lnc * LVT_rc%lnr) ! EMK
 
   prcp_out = LVT_rc%udef
   prcp_final = LVT_rc%udef
 
   if((aphroprcpobs(source)%yr.ne.LVT_rc%dyr(source)).or.&
-       LVT_rc%resetFlag(source)) then 
+       LVT_rc%resetFlag(source)) then
 
-     LVT_rc%resetFlag(source) = .false. 
+     LVT_rc%resetFlag(source) = .false.
      call ESMF_TimeSet(aphroprcpobs(source)%startTime,&
           yy=LVT_rc%dyr(source), &
           mm = 1, &
@@ -81,52 +84,73 @@ subroutine readAPHROPRCPobs(source)
 
      aphroprcpobs(source)%yr = LVT_rc%dyr(source)
 
-     if((mod(LVT_rc%dyr(source),4) .eq. 0 .and. &
-          mod(LVT_rc%dyr(source), 100).ne.0) &!leap year
-          .or.(mod(LVT_rc%dyr(source),400) .eq.0)) then
+     if ((mod(LVT_rc%dyr(source), 4) .eq. 0 .and. &
+          mod(LVT_rc%dyr(source), 100) .ne. 0) & !leap year
+          .or. (mod(LVT_rc%dyr(source), 400) .eq. 0)) then
         nt = 366
      else
         nt = 365
      endif
-     
-     allocate(rainf(aphroprcpobs(source)%nc,&
-          aphroprcpobs(source)%nr,&
+
+     allocate(rainf(aphroprcpobs(source)%nc, &
+          aphroprcpobs(source)%nr, &
           nt))
 
-    if(aphroprcpobs(source)%loc.eq."MA") then 
-       write(fyr,fmt='(i4.4)') LVT_rc%dyr(source)
-       if(LVT_rc%dyr(source)>=2007) then           ! Yeosang Yoon
+     if (aphroprcpobs(source)%loc .eq. "MA") then
+       write(fyr, fmt='(i4.4)') LVT_rc%dyr(source)
+       ! EMK...Add support for V1901
+       if (trim(aphroprcpobs(source)%version) .eq. "V1901") then
           filename = trim(aphroprcpobs(source)%odir)//&
-               '/APHRO_MA_025deg_V1101_EXR1.'//trim(fyr)//'.nc' 
+               '/APHRO_MA_025deg_V1901.'//trim(fyr)//'.nc'
        else
-          filename = trim(aphroprcpobs(source)%odir)//&
-               '/APHRO_MA_025deg_V1101.'//trim(fyr)//'.nc'
-       endif
-    endif
+          if(LVT_rc%dyr(source)>=2007) then           ! Yeosang Yoon
+             filename = trim(aphroprcpobs(source)%odir)//&
+                  '/APHRO_MA_025deg_V1101_EXR1.'//trim(fyr)//'.nc'
+          else
+             filename = trim(aphroprcpobs(source)%odir)//&
+                  '/APHRO_MA_025deg_V1101.'//trim(fyr)//'.nc'
+          end if
+       end if
+    end if
 
-     inquire(file=trim(filename), exist=file_exists) 
-     
-     if(file_exists) then 
-        write(LVT_logunit,*) '[INFO] Reading APHRO data ',&
-             trim(filename)
+     inquire(file=trim(filename), exist=file_exists)
+
+     if (file_exists) then
+        write(LVT_logunit,*) '[INFO] Reading APHRO data ', trim(filename)
 
 #if(defined USE_NETCDF3 || defined USE_NETCDF4)
-        iret = nf90_open(path=trim(filename),mode=NF90_NOWRITE,ncid=nid)
+        iret = nf90_open(path=trim(filename), mode=NF90_NOWRITE, ncid=nid)
         call LVT_verify(iret, 'Error opening file'//trim(filename))
 
-        iret = nf90_inq_varid(nid, 'precip',varid)
+        iret = nf90_inq_varid(nid, 'precip', varid)
         call LVT_verify(iret, 'Error nf90_inq_varid: precip')
 
-        iret = nf90_get_var(nid,varid, Rainf)
+        iret = nf90_get_var(nid, varid, Rainf)
         call LVT_verify(iret, 'Error nf90_get_var: precip')
-        
+
+        ! EMK Get missing value from file
+        iret = nf90_get_att(nid, varid, "_FillValue", fill_value)
+        call LVT_verify(iret, 'Error nf90_get_att: precip')
+
         iret = nf90_close(nid)
         call LVT_verify(iret, 'Error nf90_close')
 #endif
-        
+
+        ! EMK Make sure consistent missing values are used.
+        do t = 1, nt
+           do r = 1, aphroprcpobs(source)%nr
+              do c = 1, aphroprcpobs(source)%nc
+                 if (rainf(c, r, t) .eq. fill_value) then
+                    rainf(c, r, t) = LVT_rc%udef
+                 end if
+              end do
+           end do
+        end do
      endif
+
+
      aphroprcpobs(source)%rainf = LVT_rc%udef
-     aphroprcpobs(source)%rainf(:,:,1:nt) = rainf(:,:,1:nt)
+     aphroprcpobs(source)%rainf(:, :, 1:nt) = rainf(:, :, 1:nt)
      deallocate(rainf)
 
      write(LVT_logunit,*) '[INFO] Finished processing ',trim(filename)
@@ -137,54 +161,74 @@ subroutine readAPHROPRCPobs(source)
        60*LVT_rc%dmn(source) + LVT_rc%dss(source)
   alarmcheck = (mod(timenow, 86400.0).eq.0)
 
-  if(alarmCheck) then 
+  if(alarmCheck) then
      call ESMF_TimeSet(aphrotime1, yy=LVT_rc%dyr(source), &
           mm=LVT_rc%dmo(source), dd=LVT_rc%dda(source), &
           h=LVT_rc%dhr(source), m=LVT_rc%dmn(source), &
           s = LVT_rc%dss(source), calendar=LVT_calendar, rc=status)
      call LVT_verify(status, 'aphrotime1 set failed')
-     
-     k = nint((aphrotime1 - aphroprcpobs(source)%starttime)/&
+
+     k = nint((aphrotime1 - aphroprcpobs(source)%starttime) / &
           aphroprcpobs(source)%timestep)+1
-     
-     lb = .false. 
+
+     lb = .false.
      prcp_in = LVT_rc%udef
      do r=1,aphroprcpobs(source)%nr
         do c=1,aphroprcpobs(source)%nc
-           if(aphroprcpobs(source)%rainf(c,r,k).ge.0) then
+           if(aphroprcpobs(source)%rainf(c, r, k) .ge. 0) then
               prcp_in(c+(r-1)*aphroprcpobs(source)%nc) = &
-                   aphroprcpobs(source)%rainf(c,r,k)
-              lb(c+(r-1)*aphroprcpobs(source)%nc) = .true. 
+                   aphroprcpobs(source)%rainf(c, r, k)
+              lb(c+(r-1)*aphroprcpobs(source)%nc) = .true.
            endif
         enddo
      enddo
-     
-     call bilinear_interp(LVT_rc%gridDesc,lb,prcp_in, &
-          lo,prcp_out, &
-          aphroprcpobs(source)%nc*aphroprcpobs(source)%nr, &
-          LVT_rc%lnc*LVT_rc%lnr,   &
-          aphroprcpobs(source)%rlat, &
-          aphroprcpobs(source)%rlon, &
-          aphroprcpobs(source)%w11,  &
-          aphroprcpobs(source)%w12,  &
-          aphroprcpobs(source)%w21,  &
-          aphroprcpobs(source)%w22,  &
-          aphroprcpobs(source)%n11,  &
-          aphroprcpobs(source)%n12,  &
-          aphroprcpobs(source)%n21,  &
-          aphroprcpobs(source)%n22,  &
-          LVT_rc%udef, iret)     
-     
+
+     ! EMK Update interpolation to either conservative or upscale averaging
+     ! call bilinear_interp(LVT_rc%gridDesc,lb,prcp_in, &
+     !      lo,prcp_out, &
+     !      aphroprcpobs(source)%nc*aphroprcpobs(source)%nr, &
+     !      LVT_rc%lnc*LVT_rc%lnr,   &
+     !      aphroprcpobs(source)%rlat, &
+     !      aphroprcpobs(source)%rlon, &
+     !      aphroprcpobs(source)%w11,  &
+     !      aphroprcpobs(source)%w12,  &
+     !      aphroprcpobs(source)%w21,  &
+     !      aphroprcpobs(source)%w22,  &
+     !      aphroprcpobs(source)%n11,  &
+     !      aphroprcpobs(source)%n12,  &
+     !      aphroprcpobs(source)%n21,  &
+     !      aphroprcpobs(source)%n22,  &
+     !      LVT_rc%udef, iret)
+
+     if (LVT_isAtAFinerResolution(aphroprcpobs(source)%datares)) then
+        call conserv_interp(LVT_rc%gridDesc, lb, prcp_in, &
+             lo, go, &
+             aphroprcpobs(source)%nc * aphroprcpobs(source)%nr, &
+             LVT_rc%lnc*LVT_rc%lnr, &
+             aphroprcpobs(source)%rlat, aphroprcpobs(source)%rlon, &
+             aphroprcpobs(source)%w112, aphroprcpobs(source)%w122, &
+             aphroprcpobs(source)%w212, aphroprcpobs(source)%w222, &
+             aphroprcpobs(source)%n112, aphroprcpobs(source)%n122, &
+             aphroprcpobs(source)%n212, aphroprcpobs(source)%n222, &
+             LVT_rc%udef, iret)
+     else
+         call upscaleByAveraging(&
+              aphroprcpobs(source)%nc * aphroprcpobs(source)%nr, &
+              LVT_rc%lnc*LVT_rc%lnr, LVT_rc%udef, &
+              aphroprcpobs(source)%n11, lb, &
+              prcp_in, lo, go)
+      end if
+
      do r=1,LVT_rc%lnr
         do c=1, LVT_rc%lnc
            prcp_final(c,r) = prcp_out(c+(r-1)*LVT_rc%lnc)
         enddo
      enddo
-     
+
      ! Convert mm/day to kg/m2s (note that 1 mm = 1 kg/m2)
      do r=1,LVT_rc%lnr
         do c=1,LVT_rc%lnc
-           if(prcp_final(c,r).ge.0) then 
+           if(prcp_final(c,r).ge.0) then
               prcp_final(c,r) = prcp_final(c,r)/86400.0 !kg/m2s
            else
               prcp_final(c,r) = LVT_rc%udef
@@ -193,24 +237,30 @@ subroutine readAPHROPRCPobs(source)
      enddo
   endif
 
-  call LVT_logSingleDataStreamVar(LVT_MOC_rainf,source,prcp_final,vlevel=1,units='kg/m2s')
-  call LVT_logSingleDataStreamVar(LVT_MOC_rainfforc,source,prcp_final,vlevel=1,units='kg/m2s')
-  call LVT_logSingleDataStreamVar(LVT_MOC_totalprecip,source,prcp_final,vlevel=1,units='kg/m2s')
-     
+  call LVT_logSingleDataStreamVar(LVT_MOC_rainf, source, prcp_final, &
+       vlevel=1, units='kg/m2s')
+  call LVT_logSingleDataStreamVar(LVT_MOC_rainfforc, source, prcp_final, &
+       vlevel=1, units='kg/m2s')
+  call LVT_logSingleDataStreamVar(LVT_MOC_totalprecip, source, prcp_final, &
+       vlevel=1, units='kg/m2s')
+
   ! Now convert from kg/m2s to kg/m2
-  do r=1,LVT_rc%lnr
-     do c=1,LVT_rc%lnc
-        if(prcp_final(c,r).ge.0) then 
-           prcp_final(c,r) = prcp_final(c,r)*86400.0 !kg/m2
+  do r = 1, LVT_rc%lnr
+     do c = 1, LVT_rc%lnc
+        if(prcp_final(c, r) .ge. 0) then
+           prcp_final(c, r) = prcp_final(c, r)*86400.0 !kg/m2
         else
-           prcp_final(c,r) = LVT_rc%udef
+           prcp_final(c, r) = LVT_rc%udef
         endif
      enddo
   enddo
-  
-  call LVT_logSingleDataStreamVar(LVT_MOC_rainf,source,prcp_final,vlevel=1,units='kg/m2')
-  call LVT_logSingleDataStreamVar(LVT_MOC_rainfforc,source,prcp_final,vlevel=1,units='kg/m2')
-  call LVT_logSingleDataStreamVar(LVT_MOC_totalprecip,source,prcp_final,vlevel=1,units='kg/m2') 
-     
+
+  call LVT_logSingleDataStreamVar(LVT_MOC_rainf, source, prcp_final, &
+       vlevel=1, units='kg/m2')
+  call LVT_logSingleDataStreamVar(LVT_MOC_rainfforc, source, prcp_final, &
+       vlevel=1, units='kg/m2')
+  call LVT_logSingleDataStreamVar(LVT_MOC_totalprecip, source, prcp_final, &
+       vlevel=1, units='kg/m2')
+
 end subroutine readAPHROPRCPobs
 
